3a67430c2be1a86d87c919c45363288d6e5d131e
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-25 21:23:04 +0530

    changes

--- trappingRainwater.java
+++ trappingRainwater.java
@@ -3,23 +3,23 @@ public class trappingRainwater {
 
     public static int trappedRainwater(int height[]) {
         // calculate left max boundary - array
-        int size = height.length;
+        int n = height.length;
         int waterlevel, trappedWater = 0;
-        int leftMax[] = new int[size];
-        int rightMax[] = new int[size];
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
         leftMax[0] = height[0];
-        for (int i = 1; i < size; i++) {
+        for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
         // calculate right max boundary - array
-        rightMax[size - 1] = height[size - 1];
-        for (int i = size - 2; i >= 0; i--) {
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
             rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
 
         // loop
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < n; i++) {
             // waterlevel = min(leftmax boundary, rightmax boundary)
             waterlevel = Math.min(leftMax[i], rightMax[i]);
             // trappedwater = waterlevel - height[i]

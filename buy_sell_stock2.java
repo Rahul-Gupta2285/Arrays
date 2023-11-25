7d051686817ae40e54e098c214693f565864abe6
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-25 14:57:58 +0530

    changes

--- buy_sell_stock2.java
+++ buy_sell_stock2.java
@@ -3,19 +3,23 @@
 
 public class buy_sell_stock2 {
 
-    public static int buySellStock(int arr[]) {
+    public static int buySellStock(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
         int maxprofit = 0;
-        int minSoFar = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            minSoFar = Math.min(minSoFar, arr[i]);
-            int profit = arr[i] - minSoFar;
-            maxprofit = Math.max(maxprofit, profit);
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) { // profit
+                int profit = prices[i] - buyPrice; // today's profit
+                maxprofit = Math.max(maxprofit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
         }
         return maxprofit;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 7, 1, 5, 3, 6, 4 };
-        System.out.println(buySellStock(arr));
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buySellStock(prices));
     }
 }

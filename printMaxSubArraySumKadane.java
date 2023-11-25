1102b7ef50756138c73deff2bc5c872a49b198d7
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-25 10:31:23 +0530

    changes

--- printMaxSubArraySumKadane.java
+++ printMaxSubArraySumKadane.java
@@ -3,20 +3,20 @@
 
 public class printMaxSubArraySumKadane {
 
-    public static void print_max_sub_array_sum_kadane(int numbers[]) {
-        System.out.println();
-        int maxSum = Integer.MIN_VALUE, sum = 0, i;
-        for (i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+    public static int MaxSubArraySum(int arr[]) {
+        int maxSum = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
             if (sum < 0) {
                 sum = 0;
             }
             maxSum = Math.max(maxSum, sum);
         }
+        return maxSum;
     }
 
     public static void main(String[] args) {
-        int numbers[] = { -2, -3, -4, -1, -2, -1, -5, -3 };
-        print_max_sub_array_sum_kadane(numbers);
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        System.out.println(MaxSubArraySum(arr));
     }
 }

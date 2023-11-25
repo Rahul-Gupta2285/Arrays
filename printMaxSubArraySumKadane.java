29e7ff78bb6bbfbed92e89b6468fee02170d61dd
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-25 21:20:08 +0530

    changes

--- printMaxSubArraySumKadane.java
+++ printMaxSubArraySumKadane.java
@@ -1,22 +1,21 @@
 
 //time complexity of this program is O(n)
+//space complexity of this program is O(1)
 
 public class printMaxSubArraySumKadane {
 
-    public static int MaxSubArraySum(int arr[]) {
-        int maxSum = Integer.MIN_VALUE, sum = 0;
+    public static int sum(int arr[]) {
+        int sum = 0, maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
-            if (sum < 0) {
-                sum = 0;
-            }
-            maxSum = Math.max(maxSum, sum);
+            maxSum = Math.max(sum, maxSum);
+            sum = Math.max(sum, 0);
         }
         return maxSum;
     }
 
     public static void main(String[] args) {
-        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-        System.out.println(MaxSubArraySum(arr));
+        int arr[] = { -1, -2, -3, -4 };
+        System.out.println(sum(arr));
     }
 }
